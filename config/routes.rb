67c9185672094903c6f1821f5ab7399094b3dab5
Nikola-Nino <74680506+Nikola-Nino@users.noreply.github.com>
2022-04-16 07:57:58 +0200

    Updated navbar links paths

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get "/home", to: "pages#home"
   get "/contact", to: "pages#contact"
   get "/about", to: "pages#about"
-  get "/privacy", to: "pages#privacy"
+  get "/articles", to: "pages#articles"
 
   root to: "pages#home"
 end
