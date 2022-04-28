27486cde89e3d20a10764b0e748a4ed7831f4196
Nikola-Nino <74680506+Nikola-Nino@users.noreply.github.com>
2022-04-28 23:38:09 +0200

    Fixed rubocop offenses

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -3,6 +3,7 @@
 class Contact < ApplicationRecord
   validates :full_name, presence: true
   validates :email, email: { mode: :strict }
-  validates :phone, presence: true, format: { with: /^\+?\d{8,20}$/, multiline: true, message: "please enter phone number in correct format"}
+  validates :phone, presence: true,
+                    format: { with: /^\+?\d{8,20}$/, multiline: true }
   validates :message, presence: true
 end
