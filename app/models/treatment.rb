d124bd45d54d30cb73e922321c4fb666df716a5f
Nikola-Nino <74680506+Nikola-Nino@users.noreply.github.com>
2022-05-21 09:52:18 +0200

    Added treatment model validations

--- app/models/treatment.rb
+++ app/models/treatment.rb
@@ -1,3 +1,4 @@
 class Treatment < ApplicationRecord
     belongs_to :user
+    validates :full_name, :date_of_visit, :diagnosis, :treatment, :special_conditions_and_additional_notes, presence: true
 end
