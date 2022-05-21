28a7b145c61da57857dacc398fc074b0464eaf23
Nikola-Nino <74680506+Nikola-Nino@users.noreply.github.com>
2022-05-21 09:29:30 +0200

    Added treatment model and controller

---
+++ app/controllers/dashboard/treatments_controller.rb
@@ -0,0 +1,50 @@
+# frozen_string_literal: true
+
+module Dashboard
+    class TreatmentsController < Dashboard::DashboardController
+      def index
+        @treatments = current_user.treatments
+      end
+
+      def show
+      end
+
+      def new
+        @treatment = Treatment.new
+      end
+
+      def create
+        @treatment = Treatment.new(treatment_params)
+        @treatment.user_id = current_user.id
+
+        if @treatment.save
+          redirect_to  dashboard_treatments_path
+        else
+          render :new
+        end
+      end
+
+      def edit
+      end
+
+      def update
+        if @treatment.update(treatment_params)
+          redirect_to @treatment
+        else
+          render :edit
+        end
+      end
+
+      def destroy
+        @treatment.destroy
+
+        redirect_to treatments_path
+      end
+
+      private
+
+      def treatment_params
+        params.require(:treatment).permit(:full_name, :date_of_visit, :treatment, :diagnosis, :special_conditions_and_additional_notes)
+      end
+    end
+  end
