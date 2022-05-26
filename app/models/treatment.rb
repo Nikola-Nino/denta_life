f18a51e2b74b62178c54493d797a9ce14d3356f2
Nikola-Nino <74680506+Nikola-Nino@users.noreply.github.com>
2022-05-26 14:51:22 +0200

    Implement active storage

--- app/models/treatment.rb
+++ app/models/treatment.rb
@@ -2,6 +2,7 @@
 
 class Treatment < ApplicationRecord
   belongs_to :user
+  has_many_attached :images
   validates :full_name, :date_of_visit, :diagnosis, :treatment, :special_conditions_and_additional_notes,
             presence: true
 end
