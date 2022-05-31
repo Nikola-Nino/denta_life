323e45b949602bac7979df76c157d268db221274
Nikola-Nino <74680506+Nikola-Nino@users.noreply.github.com>
2022-05-31 15:52:56 +0200

    Updated view specs

--- spec/views/dashboard/treatments/new.html.erb_spec.rb
+++ spec/views/dashboard/treatments/new.html.erb_spec.rb
@@ -12,19 +12,19 @@ describe 'dashboard/treatments/new' do
                                 'Special conditions and additional notes')
   end
 
-  # it 'displays error messages' do
-  #   assign(:treatment, Treatment.create(full_name: '',
-  # date_of_visit: '',
-  # diagnosis: '',
-  # treatment: '',
-  # special_conditions_and_additional_notes: ''))
+  it 'displays error messages' do
+    assign(:treatment, Treatment.create(full_name: '',
+  date_of_visit: '',
+  diagnosis: '',
+  treatment: '',
+  special_conditions_and_additional_notes: ''))
 
-  #   render partial: 'form'
+    render partial: 'form'
 
-  #   expect(rendered).to include('Full name can&#39;t be blank')
-  #   expect(rendered).to include('Date of visit can&#39;t be blank')
-  #   expect(rendered).to include('Diagnosis can&#39;t be blank')
-  #   expect(rendered).to include('Treatment can&#39;t be blank')
-  #   expect(rendered).to include('Special conditions and additional notes can&#39;t be blank')
-  # end
+    expect(rendered).to include('Full name can&#39;t be blank')
+    expect(rendered).to include('Date of visit can&#39;t be blank')
+    expect(rendered).to include('Diagnosis can&#39;t be blank')
+    expect(rendered).to include('Treatment can&#39;t be blank')
+    expect(rendered).to include('Special conditions and additional notes can&#39;t be blank')
+  end
 end
